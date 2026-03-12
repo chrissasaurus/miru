1b077fa0af3efc8e4a0a13428ce6ea5865046c49
Chris James <chrisjames.joshy@gmail.com>
2026-03-12 22:40:36 +0530

    feat: add prisma setup with user model and pg driver adapter

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TmdbModule } from './tmdb/tmdb.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TmdbModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TmdbModule, UsersModule,PrismaModule],
   controllers: [],
   providers: [],
 })
