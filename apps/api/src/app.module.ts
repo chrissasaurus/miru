8c7a320ac967623a5a47630566264a200bef6b2c
Chris James <chrisjames.joshy@gmail.com>
2026-03-12 19:04:44 +0530

    feat: added prisma

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TmdbModule } from './tmdb/tmdb.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TmdbModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TmdbModule, UsersModule],
   controllers: [],
   providers: [],
 })
